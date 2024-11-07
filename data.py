44fb515b2b8982dbb59ce04d7719444ba052c800
ash1ra <ash1rawtf@gmail.com>
2024-11-07 12:22:42 +0200

    refactor: add check if dataset is downloaded to get rid of annoying “Files already downloaded and verified” logs

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import torch
 from torch.utils.data import DataLoader
 from torchvision import datasets
@@ -5,22 +7,24 @@ from torchvision.transforms import v2
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+ROOT_DIR = "data"
+DOWNLOAD = not (Path(ROOT_DIR) / datasets.CIFAR10.base_folder).exists()
 BATCH_SIZE = 32
 
 train_dataset_default = datasets.CIFAR10(
-    root="data",
+    root=ROOT_DIR,
     train=True,
     transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
     target_transform=None,
-    # download=True,
+    download=DOWNLOAD,
 )
 
 test_dataset_default = datasets.CIFAR10(
-    root="data",
+    root=ROOT_DIR,
     train=False,
     transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
     target_transform=None,
-    # download=True,
+    download=DOWNLOAD,
 )
 
 train_dataloader_default = DataLoader(
@@ -44,19 +48,19 @@ train_transform_v0 = v2.Compose([
 ])
 
 train_dataset_transform_v0 = datasets.CIFAR10(
-    root="data",
+    root=ROOT_DIR,
     train=True,
     transform=train_transform_v0,
     target_transform=None,
-    # download=True,
+    download=DOWNLOAD,
 )
 
 test_dataset_transform_v0 = datasets.CIFAR10(
-    root="data",
+    root=ROOT_DIR,
     train=False,
     transform=v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]),
     target_transform=None,
-    # download=True,
+    download=DOWNLOAD,
 )
 
 train_dataloader_transform_v0 = DataLoader(
